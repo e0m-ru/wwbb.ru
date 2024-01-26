f65c10f015ea76203cc710df4ed93839f5e16de7
Aleksander-e0m <e0m@me.com>
2024-01-26 16:48:44 +0300

    mv: robots.txt, sitemap.xml -> static/

--- djsite/settings.py
+++ djsite/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ea&iyv=w$f9p22q*%(^q#gp2sv&ggh!f4))uj52l1x%js!i_iw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
